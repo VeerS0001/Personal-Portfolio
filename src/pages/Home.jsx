5e3548cbf942191f91716af80feb23f2696103da
Veer Shah <159545268+VeerS0001@users.noreply.github.com>
2026-04-27 18:46:45 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
+import { Skills } from "../components/Skills";
+import { Projects } from "../components/Projects";
+import { Contact } from "../components/Contact";
+import { Footer } from "../components/Footer";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+
+      {/* Background Effects */}
+      <StarBackground />
+
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
